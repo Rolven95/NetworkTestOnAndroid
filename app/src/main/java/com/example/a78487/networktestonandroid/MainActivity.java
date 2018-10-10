0448f982a648ac698e9705d048c20f22768228e8
Ruowen Cheng <41573873+Rolven95@users.noreply.github.com>
2018-10-10 14:43:40 +0800

    main activity is the Client class

--- app/src/main/java/com/example/a78487/networktestonandroid/MainActivity.java
+++ app/src/main/java/com/example/a78487/networktestonandroid/MainActivity.java
@@ -8,15 +8,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.IOException;
+import java.net.DatagramPacket;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Context context;
+
+    //---------------------------------------
+
+    public static String serverIP = "192.168.1.105"; //"13.233.125.32";
+    public static int serverPort = 9001;
+    public static int packetLength = 512;
+    public static DatagramSocket clientSocket ;
+    public static int localPort ;
+    public static String localIP ;
+    public static boolean reqSentFlag = false;
+    public static boolean oneWayMode = true;
+    //---------------------------------------
+
     public String getIP(){
         try {
             for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
@@ -40,26 +58,135 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.new_activity_main);
         Button button = (Button)findViewById(R.id.button);
-
         TextView ipAddr = (TextView)findViewById(R.id.textview);
-        ipAddr.setText(this.getIP());
+        ipAddr.setText("Local IP: "+ this.getIP());
+        TextView cInfo = (TextView)findViewById(R.id.cInfo);
+        cInfo.setText("Showing Current Status");
         //button.setText();
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                    new Thread(){
-                        @Override
-                        public void run() {
-                            Log.i(TAG, "Thread started, try to start listener");
-                            Receiver r = new Receiver();
-                            r.server();
-                        }
-                    }.start();
+                Log.i(TAG, "Client Started");
+                try {
+                    clientSocket = new DatagramSocket();
+                    //localPort = clientSocket.getLocalPort();
+                    //localIP = InetAddress.getLocalHost().getHostAddress().toString();
+                    ExecutorService exec = Executors.newCachedThreadPool();
+                    Thread thread1=new Thread(new ClientReciever());
+                    Thread thread2=new Thread(new ClientSender());
+                    exec.execute(thread1);
+                    exec.execute(thread2);
+                    exec.shutdown();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
+
+
+
+    ///----------------------------------------------------------------------
+    static class ClientReciever implements Runnable{
+        @Override
+        public void run() {
+            try {
+                System.out.println("Clilent listener Online");
+                //System.out.println("Client listening at: "+ localIP
+                //					+ " : "+ localPort);
+                while(true) {
+                    byte[] buf = new byte[packetLength]; // The maxium size of UDP is 65507, 视线中
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    clientSocket.receive(packet);
+
+                    byte[] data = packet.getData();
+                    unicast_packet arrival = new unicast_packet();
+                    arrival = arrival.bytes_to_packet(data);
+
+                    if(arrival.getType() == -2) {//服务端收到req 返回打洞尝试
+                        oneWayMode = false;
+                        System.out.println("client in duplex mode");
+                        for(int i = 0 ;i < 5 ; i++){
+                            //byte[] buf = new byte[packetLength];
+                            unicast_packet to_sent = new unicast_packet(-2);
+                            //System.out.println("clent type = " + to_sent.getType());
+                            buf = to_sent.toByteArray();
+                            System.out.println("dup notif sent to  " + serverIP +":" + serverPort);
+                            DatagramPacket tosent = new DatagramPacket(buf, buf.length,
+                                    InetAddress.getByName(serverIP), serverPort); //192.168.202.191  192.168.109.1
+                            clientSocket.send(tosent);
+                        }
+                    }else if(arrival.getType() == 0) {//收到Data 返回ACK
+                        oneWayMode = false;
+                        //System.out.println("client enter duplex mode");
+                        unicast_packet to_sent = arrival;
+                        arrival.seType(1);
+                        arrival.setArrival(System.currentTimeMillis());
+                        buf = to_sent.toByteArray();
+                        DatagramPacket tosent = new DatagramPacket(buf, buf.length,
+                                InetAddress.getByName(serverIP), serverPort); //192.168.202.191  192.168.109.1
+                        //Thread.sleep(2000);
+                        clientSocket.send(tosent);
+                        System.out.println(arrival.getSeq() + " ACK sent back");
+                } else {
+                    System.out.println("recieved a shit");
+                }
+                }
+            }catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class ClientSender implements Runnable{
+        @Override
+        public void run() {
+            try {
+                if (!reqSentFlag) {
+                    for(int i = 0 ;i < 20 ; i++){
+                        byte[] buf = new byte[packetLength];
+                        unicast_packet to_sent = new unicast_packet(-1, -1);
+                        //System.out.println("clent type = " + to_sent.getType());
+                        buf = to_sent.toByteArray();
+                        System.out.println("req sent to " + serverIP +" at " + serverPort);
+                        DatagramPacket tosent = new DatagramPacket(buf, buf.length,
+                                InetAddress.getByName(serverIP), serverPort); //192.168.202.191  192.168.109.1
+                        clientSocket.send(tosent);
+                        Thread.sleep(3);
+                    }
+                }
+                Log.i(TAG, "Sender: Connection request sent, waiting for response (6s)");
+
+                Thread.sleep(6000);
+
+                if(oneWayMode) {
+                    Log.i(TAG, "Sender: Entered One-way mode");
+                    System.out.println("Client sender on one way mode, start send shit to server");
+                    for(int i = 0 ;i<10000 ; i++){
+                        byte[] buf = new byte[packetLength];
+                        unicast_packet to_sent = new unicast_packet(i,0);
+                        to_sent.setDeparture(System.currentTimeMillis());
+                        buf = to_sent.toByteArray();
+                        Log.d(TAG, i + " sent to " + serverIP +" at " + serverPort);
+                        System.out.println(i + " sent to " + serverIP +" at " + serverPort);
+                        DatagramPacket tosent = new DatagramPacket(buf, buf.length,
+                                InetAddress.getByName(serverIP), serverPort); //192.168.202.191  192.168.109.1
+                        clientSocket.send(tosent);
+                        //Thread.sleep(10);
+                    }
+                }
+
+            } catch (IOException | InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    //-----------------------------------------------------------------------
 }
--- app/src/main/java/com/example/a78487/networktestonandroid/unicast_packet.java
+++ app/src/main/java/com/example/a78487/networktestonandroid/unicast_packet.java
@@ -1,22 +1,48 @@
 package com.example.a78487.networktestonandroid;
 
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 import java.net.InetAddress;
+
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-/**
- * Created by 78487 on 2018/9/28.
- */
 
+//import java.lang;
 public class unicast_packet {
-    private int seq; //
-    private long departure;
-    private long arrival;
-    private long processing_cost; //so fucking helpful
-    private String from;
 
-    //System.currentTimeMillis()
+    private int seq = 0; //  if seq == -1, this is connection building packet
+
+    private long departure = 0;
+    private long arrival = 0;
+    private long nakArrival = 0; //so fucking helpful
+    private String from = "";
+    private int type = 0;
+
+    public unicast_packet() {
+
+    }
+    public unicast_packet(int t) {
+
+        type = t;
+
+    }
+    public unicast_packet(int s, int t) {
+        seq = s;
+        type = t;
+    }
+
+    public unicast_packet(int s, long d, long a, long p, String f, int t) {
+        seq = s;
+        departure = d;
+        arrival = a;
+        nakArrival = p;
+        from = f;
+        type = t;
+    }
 
     public int getSeq() {
         return seq;
@@ -36,11 +62,11 @@ public class unicast_packet {
     public void setArrival(long r) {
         this.arrival = r;
     }
-    public long getProcessing_cost() {
-        return processing_cost;
+    public long getNakArrival() {
+        return nakArrival;
     }
-    public void setProcessing_cost(long re) {
-        this.processing_cost = re;
+    public void setNakArrival(long re) {
+        this.nakArrival = re;
     }
     public String getFrom() {
         return from;
@@ -48,30 +74,32 @@ public class unicast_packet {
     public void setFrom(String ss) {
         this.from = ss;
     }
-
+    public int getType() {
+        return type;
+    }
+    public void seType(int p) {
+        this.type = p;
+    }
     public byte[] toByteArray() throws UnknownHostException {
-        ByteBuffer buffer = ByteBuffer.allocate(32);
+        ByteBuffer buffer = ByteBuffer.allocate(36);
         buffer.order(ByteOrder.BIG_ENDIAN);
+
         buffer.putInt(this.seq);	    	  // 4bytes 0-3
-        buffer.putLong(this.departure); 	  // 8bytes 4-11
-        buffer.putLong(this.arrival);    	  // 8bytes 12-19
-        buffer.putLong(this.processing_cost); // 8bytes 20-27
-        byte ip_in_bytes[] = InetAddress.getByName(this.from).getAddress();
+        buffer.putInt(this.type); // 32 33 34 35
+
+        buffer.putLong(this.departure); 	  // 8bytes 4-11 +4
+        buffer.putLong(this.arrival);    	  // 8bytes 12-19 +4
+        buffer.putLong(this.nakArrival); // 8bytes 20-27 +4
+        byte ip_in_bytes[] = InetAddress.getByName(this.from).getAddress(); //+4
         buffer.put(ip_in_bytes[0]);
         buffer.put(ip_in_bytes[1]);
         buffer.put(ip_in_bytes[2]);
         buffer.put(ip_in_bytes[3]);
+
+        //System.out.println("");
         byte[] bytes = buffer.array();
-
-        //for(int i = 0; i<32 ; i++) {
-        //	System.out.println("all["+ i +"] =  "+ bytes[i]);
-        //}
-
-        //System.out.println("dep decoded is : " + longFrom8Bytes(bytes, 4, false));
-
         return bytes;
     }
-
     public static long longFrom8Bytes(byte[] input, int offset, boolean littleEndian){
         long value=0;
         // 循环读取每个字节通过移位运算完成long的8个字节拼装
@@ -83,6 +111,7 @@ public class unicast_packet {
     }
 
     public unicast_packet bytes_to_packet (byte[] input) {
+
         unicast_packet result = new unicast_packet();
 
         int seq  = input[3] & 0xFF;
@@ -90,18 +119,40 @@ public class unicast_packet {
         seq |= ((input[1] << 16) & 0xFF0000);
         seq |= ((input[0] << 24) & 0xFF000000);
 
-        long dep = longFrom8Bytes(input, 4, false);
-        long cost = longFrom8Bytes(input, 20, false);
+        int temtype  = input[7] & 0xFF;
+        temtype |= ((input[6] << 8) & 0xFF00);
+        temtype |= ((input[5] << 16) & 0xFF0000);
+        temtype |= ((input[4] << 24) & 0xFF000000);
 
-        String from_addr = (input[28] & 0xff)
-                + "." + (input[29] & 0xff)
-                + "." + (input[30] & 0xff)
-                + "." + (input[31] & 0xff);
+        long dep = longFrom8Bytes(input, 4+4, false);
+        long cost = longFrom8Bytes(input, 20+4, false);
+
+
+        String from_addr = (input[28+4] & 0xff)
+                + "." + (input[29+4] & 0xff)
+                + "." + (input[30+4] & 0xff)
+                + "." + (input[31+4] & 0xff);
+
         result.setDeparture(dep);
-        result.setProcessing_cost(cost);
+        result.setNakArrival(cost);
         result.setSeq(seq);
         result.setFrom(from_addr);
-
+        result.seType(temtype);
         return result;
+    }
+
+    public void sendThisPacket(unicast_packet to_sent, DatagramSocket socket, String targetIP, int port){
+        try {
+            byte[] buf = to_sent.toByteArray();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                    InetAddress.getByName(targetIP), port);
+            socket.send(packet);
+            System.out.println( seq +" sent");
+            //return to_sent;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //return null;
     }
 }
